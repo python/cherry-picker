f9b37340ebf37c8a9b7edb40adefba25c16d0849
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-07-16 00:55:06 +0200

    Clarify that ``git`` ``2.28.0+`` is needed for tests (#66)
    * Clarify that ``git`` ``2.28.0+`` is needed for tests
    
    * Raise a warning
    
    * Fix bandit issue
    
    * Fix sorting
    
    * Update cherry_picker/test_cherry_picker.py
    
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>
    
    * Fix syntax
    
    Co-authored-by: Ezio Melotti <ezio.melotti@gmail.com>

--- cherry_picker/test_cherry_picker.py
+++ cherry_picker/test_cherry_picker.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import subprocess
+import warnings
 from collections import ChainMap
 from unittest import mock
 
@@ -110,7 +111,17 @@ def git_config():
 def tmp_git_repo_dir(tmpdir, cd, git_init, git_commit, git_config):
     repo_dir = tmpdir.mkdir("tmp-git-repo")
     cd(repo_dir)
-    git_init()
+    try:
+        git_init()
+    except subprocess.CalledProcessError:
+        version = subprocess.run(("git", "--version"), capture_output=True)
+        # the output looks like "git version 2.34.1"
+        v = version.stdout.decode("utf-8").removeprefix('git version ').split('.')
+        if (int(v[0]), int(v[1])) < (2, 28):
+            warnings.warn(
+                "You need git 2.28.0 or newer to run the full test suite.",
+                UserWarning,
+            )
     git_config("--local", "user.name", "Monty Python")
     git_config("--local", "user.email", "bot@python.org")
     git_config("--local", "commit.gpgSign", "false")
